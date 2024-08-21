369a73595b2caa361ac71ab36c3df0449eb2ce2d
Classiee <Kristianfratini@gmail.com>
2024-08-21 14:49:10 +0200

    Arrays oppgave 2 ferdig

--- java.js
+++ java.js
@@ -1,21 +1,33 @@
-let numbers = [1, 2, 3, 4, 5];
-for (let i = 0; i < numbers.length; i++) {
-        console.log(numbers[i]);
- }
+let products = [];
 
-function pushArray(){
-    document.getElementById("showArray").innerHTML = numbers;
+function addItem() {
+    let product = document.getElementById("input").value;
+    products.push(product);
+    document.getElementById("input").value = "";
+    
+    function updateArray() {
+        let list = document.getElementById("showArray");
+        list.innerHTML = "<h3>Her er dine valgte produkter:</h3>";
+        for (let i = 0; i < products.length; i++) {
+            list.innerHTML += "<li>" + products[i] + "</li>";
+        }
+        document.getElementById("showArray").showArray = list;
+    }
+    updateArray();
 }
 
 
-document.getElementById("app").innerHTML = `
+
+ document.getElementById("app").innerHTML = `
         
             <div id="title">
                 <h1>Array Loop</h1>
             </div>
             
-            <button onclick="pushArray()" id="showArray">Vis arrayen</button>
+            <input type="text" id="input" placeholder="Hva ønsker du?">
+            <button onclick="addItem()" id="button">Add to Cart</button>
+            <div id="showArray"></div>
+            `
             
         
         
-`
